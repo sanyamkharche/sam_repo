d21e0cdaf078b49c3db27b9d6eb361561c902079
sanyamkharche <sanyamkharche>
2018-10-15 16:56:56 +0530

    adding generics

---
+++ Generics/src/com/snk/pack1/Gen.java
@@ -0,0 +1,17 @@
+package com.snk.pack1;
+//Our own defined generic class
+public class Gen<T> {
+	T ob;
+	Gen(T ob)
+	{
+		this.ob = ob;
+	}
+	public void show()
+	{
+		System.out.println("The type of ob:"+ob.getClass().getName());
+	}
+	public T getOb()
+	{
+		return ob;
+	}
+}
